2f0135665981d0ac0c314a994a0a52db22ebe2f4
Afif13 <temani.afif@gmail.com>
2023-04-05 17:13:54 +0200

    fix favicon

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,7 +1,7 @@
 module.exports = {
 	"title": "CSS Tip: Learn CSS the easy one",
 	"url": "https://css-tip.com/",
-	"image" : "https://css-tip.com/css-tip.jpg",
+	"image" : "/img/css-tip.jpg",
 	"language": "en",
 	"description": "A wide collection of CSS tips and tricks. The best place to keep up to date with the new CSS features",
 	"author": {
