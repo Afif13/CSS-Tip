1032897ac17a99fdaf9dd817de99ce1a8a8195d1
Afif13 <temani.afif@gmail.com>
2024-06-23 01:21:00 +0200

    fix title and desc

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,9 +1,9 @@
 module.exports = {
-	"title": "CSS Tip: Learn CSS the easy one",
+	"title": "CSS Tip: Learn CSS the easy way",
 	"url": "https://css-tip.com/",
 	"image" : "https://css-tip.com/img/css-tip.jpg",
 	"language": "en",
-	"description": "A wide collection of CSS tips and tricks. The best place to keep up to date with the new CSS features",
+	"description": "The best place to keep up to date with the new CSS features. Daily CSS tips and tricks to become a better web developer.",
 	"author": {
 		"name": "Temani Afif",
 	}
